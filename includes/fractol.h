e15a09b3a1bf846693087f356a7bac99d63215e4
Simos Tigkas <stigkas@c1r6p8.hive.fi>
2024-02-23 19:13:38 +0200

    lesssgooo

--- includes/fractol.h
+++ includes/fractol.h
@@ -3,31 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/21 19:08:09 by marvin            #+#    #+#             */
-/*   Updated: 2024/02/21 19:08:09 by marvin           ###   ########.fr       */
+/*   Created: 2024/02/22 08:32:38 by stigkas           #+#    #+#             */
+/*   Updated: 2024/02/22 08:32:38 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include "includes/libft.h"
+# include "libft.h"
 # include <unistd.h>
 # include <math.h>
+# include "../MLX42/include/MLX42/MLX42.h"
+# include <stdio.h>
+# include <stdlib.h>
 
 typedef struct s_complex
 {
-    double  real;
-    double  imgnry;
+    double x;
+    double y;
 }               t_complex;
 
-#define ERROR_MESSAGE "Please enter \n\t\"./fractol mandelbrot\" or \n\t\"./fractol julia <value1> <value2>\"
-
-
-
-
-
+# define ERROR_MESSAGE "./fractol mandelbrot or ./fractol julia <v1> <v2>"
+# define WIDTH 1028
+# define HEIGHT 1028
+# define BLACK       0x000000
+# define WHITE       0xFFFFFF
+# define RED         0xFF0000
+# define GREEN       0x00FF00
+# define BLUE        0x0000FF
 
 #endif
--- main.c
+++ main.c
@@ -5,24 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/22 08:33:05 by stigkas           #+#    #+#             */
-/*   Updated: 2024/02/22 08:33:05 by stigkas          ###   ########.fr       */
+/*   Created: 2024/02/22 08:37:46 by stigkas           #+#    #+#             */
+/*   Updated: 2024/02/23 16:40:59 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inlcudes/fractol.h"
+#include "includes/fractol.h"
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    if ((ac == 2 && !ft_strncmp(av[1], "mandelbrot", 10))
-        || (ac == 4 ) && ft_strcmp(av[1], "julia", 5))
-    {
+	if ((ac == 2 && !ft_strncmp(av[1], "mandelbrot", 10))
+		|| ((ac == 4) && ft_strncmp(av[1], "julia", 5)))
+	{
         //todo
-    }
-    else
-    {
-        ft_putstr_fd(ERROR_MESSAGE, 2);
-        exit(EXIT_FAILURE);
-    }
-    return (0);
+	}
+	else
+	{
+		ft_putstr_fd(ERROR_MESSAGE, 2);
+		exit(EXIT_FAILURE);
+	}
+	return (0);
 }
